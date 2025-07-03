ac0346def7fd1d9e0c7b91c294ef753b42b99d44
Ankudar <ankudar@gmail.com>
2025-07-03 14:21:00 +0300

    Некоторые правки

--- dismissal_predict/prepare_dataset.py
+++ dismissal_predict/prepare_dataset.py
@@ -7,10 +7,9 @@ DATA_INTERIM = "data/interim"
 
 
 def process_dataset(input_file, output_dir):
-    # Чтение файла Excel, указывая, что заголовки находятся в первой строке
+    # Чтение файла Excel, с указанием строки с заголовками если они не в первой
     df = pd.read_excel(input_file, header=2)
 
-    # Вывод имен столбцов для проверки
     print("Имена столбцов в файле:", df.columns.tolist())
 
     # Выбор нужных столбцов по именам
@@ -25,10 +24,7 @@ def process_dataset(input_file, output_dir):
         "ОТДЕЛ",
     ]
 
-    # Удаление лишних пробелов из имен столбцов
     df.columns = df.columns.str.strip()
-
-    # Проверка наличия нужных столбцов
     missing_columns = [col for col in columns_to_copy if col not in df.columns]
     if missing_columns:
         print("Отсутствуют столбцы:", missing_columns)
@@ -36,16 +32,10 @@ def process_dataset(input_file, output_dir):
 
     processed_data = df[columns_to_copy]
 
-    # Формирование пути к выходному файлу
     output_file = os.path.join(output_dir, "check_last_users_update.csv")
-
-    # Сохранение в формате CSV
     processed_data.to_csv(output_file, index=False, encoding="utf-8")
     print("Файл успешно обработан и сохранен.")
 
 
 if __name__ == "__main__":
-    # Создание папки, если она не существует
-    os.makedirs(DATA_INTERIM, exist_ok=True)
-
     process_dataset(INPUT_FILE_CADR, DATA_INTERIM)
